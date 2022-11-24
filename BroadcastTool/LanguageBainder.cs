ea9b52dec1a6d13a02635de4b27a92d8122f36b4
Seaoftrees08 <tasuzuya@gmail.com>
2022-11-24 11:13:45 +0900

    プロジェクト ファイルを追加します。

---
+++ BroadcastTool/LanguageBainder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BroadcastTool
+{
+    internal class LanguageBainder
+    {
+
+        /// <summary>
+        /// 将来的に複数の言語に対応するためのもの
+        /// </summary>
+        /// <param name="language"></param>
+        /// <param name="main"></param>
+        public static void textBinding(Language language, MainWindow main)
+        {
+            //main.DataContext = new { File = Directory.GetParent(Assembly.GetEntryAssembly().Location) };
+        }
+
+        /// <summary>
+        /// EnumクラスのLanguage(int)を言語のstringへ変換します
+        /// これはファイル参照時等に使います
+        /// </summary>
+        /// <param name="lng">Language</param>
+        /// <returns>言語のstring</returns>
+        private static string languageToString(Language lng)
+        {
+            switch (lng)
+            {
+                case Language.English:
+                    return "en-us";
+                case Language.Japanese:
+                    return "ja-jp";
+                default:
+                    return "en-us";
+            }
+        }
+
+    }
+
+    public enum Language{
+        Japanese,   //0
+        English     //1
+    }
+}
